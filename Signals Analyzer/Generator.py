75702a6d1ef50fb987ab1a87e3c30fb7e8af894d
harivegi47-art <harivegi47@gmail.com>
2026-06-27 23:37:02 +0530

    Added sine function to Generator file

--- Signals Analyzer/Generator.py
+++ Signals Analyzer/Generator.py
@@ -2,3 +2,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def sine(frequency, amplitude, phase=0, duration=1, sample_space=100):
+    # Generates a Sine wave with the given frequency, amplitude, phase (default 0), duration (default 1 sec) and sample space (default 100)
+    # Stores it as a numpy array
+    timestamps = np.linspace(0,duration,sample_space)
+    SineWave = amplitude * np.sin(2 * np.pi *frequency * timestamps + phase)
+    return SineWave
