6dc57bf73ffd563189383c8c170ca26aa9e7fffb
harivegi47-art <harivegi47@gmail.com>
2026-09-03 18:21:50 +0530

    feat(logic files): add functions and updates readme.md - change padding code in class N_Bits - add class BCD to bits.py - add class BCD section to readme file of logic files

--- PyDigitalLab/Logic files/README.md
+++ PyDigitalLab/Logic files/README.md
@@ -23,3 +23,19 @@ Bit = N_Bits(5, 4)
 
 - Any bit can be reassigned using `obj[index] = value`; the internal `value` attribute is automatically updated.  
   Example: `Bit[3] = 0` changes the bit pattern to `[0, 1, 0, 0]` and updates `Bit.value` to `4`.
+
+### class BCD
+This class takes a numerical value and converts it into BCD number representation. Each digit of the input value will be stored as a 4 bit nibble. This class supports 2 digit integers, and if something else is given as input, ValueError is raised. The BCD nibbles are stored as strings in a python array.
+
+Example:
+```python
+bcd_num = BCD(25)
+```
+- Printing the object prints the array of BCD nibbles
+  Example: `print(bcd_num)` → `['0010', '0101']`
+
+- Each bit can be accessed using `obj[index]`.  
+  Example: `bcd_num[1]` → `0101`
+
+- Any bit can be reassigned using `obj[index] = value`; the internal `value` attribute is automatically updated.  (Input given as string)
+  Example: `bcd_num[1] = 0` changes the nibble to `"0010"` and updates `bcd_num.value` to `22`.
--- PyDigitalLab/Logic files/bits.py
+++ PyDigitalLab/Logic files/bits.py
@@ -9,8 +9,8 @@ class N_Bits(): #Takes a value and creates a BitVector for it.
         if len(self.vector) > width:
             raise ValueError(f"The given value can't be stored in {width} bits")
         else:
-            for i in range(width - len(self.vector)):
-                self.vector = [0] + self.vector  # Adds the remaining slots with 0 (to make the bitvector of required width)
+            padding = [0] * (width - len(bin(value)[2:]))
+            self.vector = padding + self.vector  # Adds the remaining slots with 0 (to make the bitvector of required width)
 
     def __str__(self):
         return f"{self.vector}"
@@ -23,3 +23,33 @@ class N_Bits(): #Takes a value and creates a BitVector for it.
         self.value = 0
         for b in self.vector:
             self.value = (self.value << 1) | b
+
+#Then we have BCD Representation of numbers.
+class BCD:     #basic implementation done, check for methods.
+    def __init__(self, value):
+        try:
+            num = int(value)
+        except (ValueError, TypeError):
+            raise ValueError("Invalid BCD: Input for BCD can only be an integer")
+        if num < 0 or num > 99:
+            raise ValueError("Class BCD only supports 2 digit integers.")
+        self.value = num
+        self.digits = [int(i) for i in str(num)]
+        if len(self.digits) < 2:
+            self.digits = [0]*(2-len(self.digits)) + self.digits
+        self.bcd = [(str(bin(i)[2:])).zfill(4) for i in self.digits]
+
+    def __str__(self):
+        return f"{self.bcd}"
+
+    def __getitem__(self, key):
+        return self.bcd[key]
+
+    def __setitem__(self, key, val):
+        if len(val) != 4 or not all(a in '01' for a in val):
+            raise ValueError("Invalid value: Value should be a 4-digit integer")
+        self.bcd[key] = val
+        if int(val, 2) > 9:
+            raise ValueError("Invalid BCD Digit: Value must be between 1001 and 0000 inclusive")
+        self.digits[key] = int(val,2)
+        self.value = self.digits[0]*10 + self.digits[1]
